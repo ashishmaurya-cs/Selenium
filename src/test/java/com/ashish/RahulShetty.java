601525c4e19e697153089335858a8f38c6c39715
Ram Ashish Maurya <ramashishmaurya1997@gmail.com>
2025-01-17 15:53:06 +0530

    RahulShetty

--- src/test/java/com/ashish/RahulShetty.java
+++ src/test/java/com/ashish/RahulShetty.java
@@ -1,15 +1,20 @@
 package com.ashish;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class RahulShetty {
+	
+	  // Create a logger instance
+    private static final Logger logger = LogManager.getLogger(RahulShetty.class);
 	@Test
 	public void setUp(){
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ram Ashish Maurya\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		logger.info("Browser launched...");
 		driver.manage().window().maximize();
 	}
 }
